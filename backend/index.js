b1d4d73d96db47afcc56475e6e37f7e5f48ee15e
Victor Torres <vatorresm88@gmail.com>
2019-03-01 16:08:22 -0500

    Trabajando con Middlewares

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,25 @@
 const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
 
 //Initializations
 const app = express();
 
 //Settings
 app.set('port', 4200);
+
+//Middlewares
+app.use(morgan('dev'));
+const storage = multer.diskStorage({
+    destination: path.join(__dirname, 'public/uploads'),
+    filename(req, file, cb) {
+        cb(null, new Date().getTime() + path.extname(file.originalname));
+    }
+})
+app.use(multer(storage).single('image'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //Start the server
 app.listen(app.get('port'), () => {
