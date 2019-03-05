bab82e957b0b286e9e350eed981f5f86f0373923
Victor Torres <vatorresm88@gmail.com>
2019-03-05 17:02:05 -0500

    REST API Nodejs & Mongodb

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,6 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
 
-
-
 const express = require('express');
 const morgan = require('morgan');
 const multer = require('multer');
